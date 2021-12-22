773718cb87409c454d42a4502160729257fd71a2
kokorirko <gregoire.richard@hetic.net>
2021-12-22 12:59:19 +0100

    import fonts, i18n

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    
+    <div class="header">
+      <h1 class="mainTitle">Portfolio</h1>
+      <h2 class="name">Floriane Ryan</h2>
+    </div>
+    <div class="menuContainer">
+      <!-- tree stuff-->
+    </div>
+    <Navbar />
+    <div class="aboutContainer"></div>
+    <Navbar />
+    <div class="designContainer"></div>
+    <Navbar />
+    <div class="motionDesignContainer"></div>
+    <Navbar />
+    <div class="contact"></div>
+
   </div>
 </template>
 
